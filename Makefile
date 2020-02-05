56eaa156859b7d33f33b31f2c96792adae54e430
rafalcode <ramonfallon@gmail.com>
2020-02-05 22:27:54 +0000

    lou2 accepts uneven number of rows

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ SPECINC=-I/home/nutria/mylocal/include
 SPECINC2=`pkg-config --cflags cairomm-1.0`
 SPECLIBS2=`pkg-config --libs cairomm-1.0`
 
-EXECUTABLES=binodot binind bini2 bini3 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 six b13 b14 doto smasha2 scalin dric3 td2 rcol hsric2 scalin2 tdisp expti0 expti1 randju0 expti2 jura0 jura0_d ptinsq0 rcol2 csvrd rcol rcol3 lou0 lou1
+EXECUTABLES=binodot binind bini2 bini3 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 six b13 b14 doto smasha2 scalin dric3 td2 rcol hsric2 scalin2 tdisp expti0 expti1 randju0 expti2 jura0 jura0_d ptinsq0 rcol2 csvrd rcol rcol3 lou0 lou1 crhmap lou2
 
 # small tiny shapes for testing on eigbirds. Tried to not make this output interlaced PNG files
 # but cairo docs explain that this also is a toy API, no control over that.
@@ -145,12 +145,17 @@ lou0: lou0.c
 	${CC} ${CFLAGS} -o $@ $^ ${SPECLIBS}
 lou1: lou1.c
 	${CC} ${CFLAGS} -o $@ $^ ${SPECLIBS}
+lou2: lou2.c
+	${CC} ${CFLAGS} -o $@ $^ ${SPECLIBS}
 # what lou1 can do: it takes aibsolute horizontal and vert sizes for
 # an object and gives you nrows of them, and ncols, all equally laid out
 # with space for margins.
 # How it still doesn't have text, and only copes with even nrows and even ncols
 # When considering unevens, actually 3 situations arise. nrows is uneven, ncols
 # is uneven, and both are uneven.
+# colour heatmap
+crhmap: crhmap.c
+	${CC} ${CFLAGS} -o $@ $^ ${SPECLIBS}
 
 # Just display time
 tdisp: tdisp.c
---
+++ lou2.c
@@ -0,0 +1,218 @@
+/* fit rectangles */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <cairo/cairo.h>
+const float FONTSZ=10.0; /* the size (number of colours) that our colorscheme has */
+
+typedef struct 
+{
+    float stx, sty;
+} pla_t; /* placement */
+
+void gethcol(float val, float *rgb) // get a heat colour.
+{
+  // int aR = 0, aG = 0, aB=255;  // RGB for our 1st color (blue in this case).
+  // int bR = 255, bG = 0, bB=0;    // RGB for our 2nd color (red in this case).
+  // Two colours, a and b: aR is the red chanell of a, etc.
+  float aR = 1.0, aG = .0, aB=.0;
+  float bR = .0, bG = 1., bB=.0;    // RGB for our 2nd color (red in this case).
+
+  *rgb   = (float)(bR - aR) * val + aR;      // Evaluated as -255*value + 255.
+  *(rgb+1) = (float)(bG - aG) * val + aG;      // Evaluates as 0.
+  *(rgb+2)  = (float)(bB - aB) * val + aB;      // Evaluates as 255*value + 0.
+}
+
+void gethcol2(float val, float *rgb) // get a heat colour.
+{
+  // int aR = 0, aG = 0, aB=255;  // RGB for our 1st color (blue in this case).
+  // int bR = 255, bG = 0, bB=0;    // RGB for our 2nd color (red in this case).
+  float aR = 1.0, aG = .0, aB=.0;
+  float bR = .0, bG = 1., bB=.0;    // RGB for our 2nd color (red in this case).
+
+  float rval;
+  if( val>0.5) {
+      rval=2*(val-.5);
+      *rgb   = (float)(bR - aR) * rval + aR;      // Evaluated as -255*value + 255.
+      *(rgb+1) = (float)(bG - aG) * rval + aG;      // Evaluates as 0.
+      *(rgb+2)  = (float)(bB - aB) * rval + aB;      // Evaluates as 255*value + 0.
+  } else {
+      *rgb   = 0.3;
+      *(rgb+1) = 0.3;
+      *(rgb+2)  = 0.3;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc!=5) {
+        printf("lou2. four args 1) height of rect 2) width of rect 3) nrows 4( ncols.");
+        exit(EXIT_FAILURE);
+    }
+    int rhsz=atoi(argv[1]);
+    int rvsz=atoi(argv[2]);
+    int nrow=atoi(argv[3]);
+    int ncol=atoi(argv[4]);
+
+    int i, nij, nij2, j;
+    int lmar =40, rmar =10, tmar =60, bmar =30;
+    int imwidth=1024, imheight =768;
+    cairo_surface_t *surface;
+    cairo_t *cr;
+    surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, imwidth, imheight);
+    cr = cairo_create (surface);
+    cairo_rectangle (cr, 0, 0, imwidth, imheight);
+    cairo_set_source_rgba (cr, 0, 0, 0, 0.95);
+    cairo_fill (cr);
+
+    unsigned char unevr=0; // uneven rows.
+    if(nrow%2)
+        unevr=1;
+    int totv=nrow*ncol;
+    float *vala=malloc(totv*sizeof(float));
+    for(i=0;i<totv;++i)
+        vala[i] = (float)i/(totv-1);
+    int totv3=totv*3;
+    float *rgba=malloc(totv3*sizeof(float));
+    for(i=0;i<totv;i++)
+        gethcol(vala[i], rgba+3*i);
+    for(i=0;i<totv;i++)
+        printf("%4.2f/%4.2f/%4.2f ", rgba[3*i], rgba[3*i+1], rgba[3*i+2]);
+    printf("\n"); 
+
+    int nc2=ncol/2, nr2=nrow/2;
+    char nstr[32]={0}; // numstrings.
+
+    /* we have an area within the image */
+    int awid = imwidth - lmar -rmar; /* (workable) area */
+    int ahei = imheight - tmar -bmar;
+    cairo_set_source_rgba(cr, .9, .0, .7, 0.9);
+    cairo_set_line_width (cr, 0.5);
+    cairo_rectangle (cr, lmar, tmar, awid, ahei);
+    cairo_stroke(cr);
+
+    pla_t cp; // centrepoint
+    cp.stx=lmar+awid/2;
+    cp.sty=tmar+ahei/2;
+
+    float bframe = 10; /* frame around each block */
+    float bf2 = bframe/2;
+    cairo_set_source_rgba(cr, .0, 0.8, .0, 0.8);
+    cairo_set_line_width (cr, 0.75); /* thinnest really possible */
+    /* OK, now, text consideration */
+    cairo_select_font_face (cr, "Monospace", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size (cr, FONTSZ);
+    cairo_font_extents_t fe;
+    cairo_font_extents(cr, &fe);
+    cairo_text_extents_t te;
+    float rmp[2]; // rectangle mid point
+
+    /* We're going to take it in 4 quadrants: each quadrant will be overwritten each time */
+    pla_t *tbu=calloc(nr2*nc2, sizeof(pla_t)); /* plus y */
+    for(i=0;i<nr2;++i) {
+        for(j=0;j<nc2;++j) {
+            nij = (nr2-1-i)*nc2+j;
+            nij2 = (nc2)*(nr2-i)+nij;
+            tbu[nij].stx=cp.stx + bf2 + j*(bframe+rhsz);
+            tbu[nij].sty=cp.sty - bf2 - rvsz - unevr*(bf2 + rvsz/2) - i*(bframe+rvsz);
+            cairo_set_source_rgba(cr, rgba[3*nij2], rgba[3*nij2+1], rgba[3*nij2+2], 0.8);
+            cairo_rectangle (cr, tbu[nij].stx, tbu[nij].sty, rhsz, rvsz);
+            cairo_fill(cr);
+            sprintf(nstr, "%2.1f%%", 100*vala[nij2]);
+            sprintf(nstr, "%i", nij2);
+            rmp[0] = tbu[nij].stx+rhsz/2;
+            rmp[1] = tbu[nij].sty+rvsz/2;
+            cairo_text_extents(cr, nstr, &te);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_set_source_rgba(cr, .7, .7, .7, .8);
+            cairo_show_text(cr, nstr);
+        }
+    }
+    if(unevr) {
+        for(j=0;j<ncol;++j) {
+            nij = j;
+            nij2 = ncol*nr2+ nij;
+            tbu[nij].stx=cp.stx - nc2*(bframe+rhsz) + bf2 + j*(bframe+rhsz);
+            tbu[nij].sty=cp.sty - rvsz/2;
+            cairo_set_source_rgba(cr, rgba[3*nij2], rgba[3*nij2+1], rgba[3*nij2+2], 0.8);
+            cairo_rectangle (cr, tbu[nij].stx, tbu[nij].sty, rhsz, rvsz);
+            cairo_fill(cr);
+            sprintf(nstr, "%2.1f%%", 100*vala[nij2]);
+            sprintf(nstr, "%i", nij2);
+            rmp[0] = tbu[nij].stx+rhsz/2;
+            rmp[1] = tbu[nij].sty+rvsz/2;
+            cairo_text_extents(cr, nstr, &te);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_set_source_rgba(cr, .7, .7, .7, .8);
+            cairo_show_text(cr, nstr);
+        }
+    }
+    for(i=0;i<nr2;++i) {
+        for(j=0;j<nc2;++j) {
+            nij = i*nc2+j;
+            nij2 = unevr*ncol + ncol*nr2+ (nc2)*(i+1) + nij;
+            tbu[nij].stx=cp.stx + bf2 + j*(bframe+rhsz);
+            tbu[nij].sty=cp.sty + bf2 + unevr*(bf2 + rvsz/2) + i*(bframe+rvsz);
+            cairo_set_source_rgba(cr, rgba[3*nij2], rgba[3*nij2+1], rgba[3*nij2+2], 0.8);
+            cairo_rectangle (cr, tbu[nij].stx, tbu[nij].sty, rhsz, rvsz);
+            cairo_fill(cr);
+            sprintf(nstr, "%2.1f%%", 100*vala[nij2]);
+            sprintf(nstr, "%i", nij2);
+            rmp[0] = tbu[nij].stx+rhsz/2;
+            rmp[1] = tbu[nij].sty+rvsz/2;
+            cairo_text_extents(cr, nstr, &te);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_set_source_rgba(cr, .7, .7, .7, .8);
+            cairo_show_text(cr, nstr);
+        }
+    }
+    for(i=0;i<nr2;++i) {
+        for(j=0;j<nc2;++j) {
+            nij = i*nc2+j;
+            nij2 = unevr*ncol + ncol*nr2 + i*ncol+(nc2-j-1);
+            tbu[nij].stx=cp.stx - bf2 - rhsz - j*(bframe+rhsz);
+            tbu[nij].sty=cp.sty + bf2 + unevr*(bf2 + rvsz/2) + i*(bframe+rvsz);
+            cairo_set_source_rgba(cr, rgba[3*nij2], rgba[3*nij2+1], rgba[3*nij2+2], 0.8);
+            cairo_rectangle (cr, tbu[nij].stx, tbu[nij].sty, rhsz, rvsz);
+            cairo_fill(cr);
+            sprintf(nstr, "%2.1f%%", 100*vala[nij2]);
+            sprintf(nstr, "%i", nij2);
+            rmp[0] = tbu[nij].stx+rhsz/2;
+            rmp[1] = tbu[nij].sty+rvsz/2;
+            cairo_text_extents(cr, nstr, &te);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_set_source_rgba(cr, .7, .7, .7, .8);
+            cairo_show_text(cr, nstr);
+        }
+    }
+    for(i=0;i<nr2;++i) {
+        for(j=0;j<nc2;++j) {
+            nij = (nr2-1-i)*nc2+j;
+            nij2 = (nr2-1-i)*ncol+(nc2-j-1);
+            tbu[nij].stx=cp.stx - bf2 - rhsz - j*(bframe+rhsz);
+            tbu[nij].sty=cp.sty - bf2 - rvsz - unevr*(bf2 + rvsz/2) - i*(bframe+rvsz);
+            cairo_set_source_rgba(cr, rgba[3*nij2], rgba[3*nij2+1], rgba[3*nij2+2], 0.8);
+            cairo_rectangle (cr, tbu[nij].stx, tbu[nij].sty, rhsz, rvsz);
+            cairo_fill(cr);
+            sprintf(nstr, "%2.1f%%", 100*vala[nij2]);
+            sprintf(nstr, "%i", nij2);
+            rmp[0] = tbu[nij].stx+rhsz/2;
+            rmp[1] = tbu[nij].sty+rvsz/2;
+            cairo_text_extents(cr, nstr, &te);
+            cairo_move_to (cr, rmp[0] - te.x_bearing - te.width/2, rmp[1] - fe.descent + 1+fe.height/2);
+            cairo_set_source_rgba(cr, .7, .7, .7, .8);
+            cairo_show_text(cr, nstr);
+        }
+    }
+
+    cairo_surface_write_to_png (surface, "lou2.png");
+    free(tbu);
+    free(rgba);
+    free(vala);
+    cairo_destroy (cr);
+    cairo_surface_destroy (surface);
+
+    return 0;
+}
